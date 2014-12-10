636c40153e3dbffd777ff3d71f7e6da7655fbc76
nizamsys <aa6my@qq.com>
2014-12-10 12:56:19 +0800

    Create Api_auth.php
    This is libraries for codeigniter rest server

---
+++ Api_auth.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Api_auth extends CI_Controller
+{
+    private $CI;
+    public function login($username, $password) 
+    {
+         $CI =& get_instance();
+         $CI->load->model('MY_model');
+         $CI->load->library('encrypt');
+                
+        $where = array('email' => $username);
+        $customer = $CI->MY_model->get_specified_row("system_users",$where,false,false, false);
+
+        if(!empty($customer)){ //if data exist
+             
+             if($CI->encrypt->sha1($password . $customer['salt']) === $customer['password']) //if password match with the database
+             {
+                return true;   
+                      
+             }
+             else
+             {
+                return false;           
+             }   
+        
+        }
+    }
+
+       
+
+}
+
+/* End of file api_auth.php */
+/* Location: ./application/libraries/api_auth.php */
